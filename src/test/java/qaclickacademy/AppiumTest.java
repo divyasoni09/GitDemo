56553e9ab30abe771db25347b49ab00f5aa13d36
Divya Soni <sonidivya048@gmail.com>
2021-08-02 11:38:29 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test 
+	public void NativeAPPAndroid() {
+		System.out.println("NativeAPPAndroid");
+	}
+	
+ @Test
+ public void  IOSApps() {
+	 System.out.println("IOSApps");
+ }
+}
