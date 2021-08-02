fb7afb1861f0bc954fb7afef14452472c94e62f3
Divya Soni <sonidivya048@gmail.com>
2021-08-02 13:23:58 +0530

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,6 +10,15 @@ public class RESTAPITest {
 		System.out.println("postJIRA");
 		System.out.println("postJIRA2");
 		System.out.println("postjira3");
+		//force 
+		//ffg
+		//wfjm
+		System.out.println("postJIRA4");
+		System.out.println("postJIRA5");
+		System.out.println("postjira6");
+		System.out.println("postJIRA7");
+		System.out.println("postJIRA8");
+		System.out.println("postjira9");
 		
 	}
 	
